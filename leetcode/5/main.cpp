e923dad6f54b0cf00d66093b121ee1a09c06af31
alomerry.wu <alomerry.wu@maiscrm.com>
2020-07-25 18:46:17 +0800

    leetcode 6,7

--- leetcode/5/main.cpp
+++ leetcode/5/main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-//grpcurl -plaintext -d '{"user":{"id":"5f1690b0c8418a01af40ac22"},"name":"test","type":"event","filterType":"all","tags":[],"repeatable":false,"group":[],"event":{"eventId":"maievent-app-enter"},"workflows":[],"status":"unfinishdraft"}' 127.0.0.1:1706 mairpc.marketing.MarketingService.UpsertMarketo
+
 string longestPalindrome(string s) {
     int max = -1, a, b, c, d, size = s.size();
     for (int i = 0; i < size; ++i) {
